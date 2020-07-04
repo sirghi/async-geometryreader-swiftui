e3a1e736eb3960e08b38a8fdac6ab4d17048df4e
Sergiu Todirascu <sergiu.todirascu@endava.com>
2020-07-04 12:44:30 +0300

    Initial Commit

---
+++ Countries/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Countries
+//
+//  Created by Sergiu Todirascu on 7/4/20.
+//  Copyright © 2020 Sergiu Todirascu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
